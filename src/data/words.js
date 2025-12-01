361dfcbac06ec3e52ed3044681e09f665991d09d
Gabriel Penna <gabriel.penna@tce.mg.gov.br>
2025-12-01 13:47:55 -0300

    Projeto SecretWord

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordlist = {
+
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "koenigsegg", "Lada", "ssangyong"],    
+    fruta: ["Banana", "Maçã", "Laranja", "Pera", "Uva","Caju","Goiaba","Abacaxi"],    
+    corpo: ["Braço", "Perna", "Coração", "Cérebro", "Olho","Tibia","Fêmur","Úmero"],    
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa-mãe","Processador","Mousepad","Notebook"],
+    programação: ["Linguagem", "Framework", "Variável", "Função", "Objeto","React","Angular"],    
+    alimento: ["Ensopado", "Ervilha", "Carne", "Macarrão", "Verdura"],
+};
