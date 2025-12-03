eeb979aabd3614ee91b756111008c70ecb2d2f1c
Gabriel Penna <gabriel.penna@tce.mg.gov.br>
2025-12-03 13:33:14 -0300

    correção ao vencer o jogo

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,12 +1,25 @@
 import './GameOver.css';
 
 const GameOver = ({retry,score}) => {
-  return (
-  <div>
-     <h1>Fim do Jogo!</h1> 
-     <h2>A sua pontuação foi: <span>{score}</span></h2>
-  <button onClick={retry}>Resetar jogo</button></div>
-  );
-};
+ 
+  if (score === 700) {
+    return (
+      <div className="gameover">
+        <h1>Parabéns! Você venceu o jogo!</h1>
+        <h2>Sua pontuação final foi: <span>{score}</span></h2>
+        <button onClick={retry}>Jogar novamente</button>
+      </div>
+    );
+  }   
 
+else {
+  return (
+    <div className="gameover">
+      <h1>Fim de jogo!</h1>
+      <h2>Sua pontuação final foi: <span>{score}</span></h2>
+      <button onClick={retry}>Tentar novamente</button>
+    </div>
+  );
+}
+};
 export default GameOver;
--- src/data/words.js
+++ src/data/words.js
@@ -1,9 +1,9 @@
 export const wordlist = {
 
-    Marca_carro: ["FIAT", "RENAULT", "Toyota", "Mercedes", "Volkswagen", "koenigsegg", "Lada", "ssangyong"],    
-    fruta: ["Banana", "Maçã", "Laranja", "Pera", "Uva","Caju","Goiaba","Abacaxi"],    
-    corpo: ["Braço", "Perna", "Coração", "Cérebro", "Olho","Tibia","Fêmur","Úmero"],    
+    Marca_carro: ["FIAT", "RENAULT", "Toyota", "Mercedes", "Volkswagen", "CITROEN", "HYUNDAI", "KIA"],    
+    fruta: ["LICHIA", "BLUEBERRY", "LIMÃO", "MORANGO", "Uva","Caju","Goiaba","Abacaxi"],    
+    corpo: ["CABEÇA", "UNHA", "Coração", "Cérebro", "Olho","Tibia","Fêmur","DEDO"],    
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa-mãe","Processador","Mousepad","Notebook"],
     programação: ["Linguagem", "Framework", "Variável", "Função", "Objeto","React","Angular"],    
-    alimento: ["Fettuccine", "Parmegiana", "Carne", "Hamburguer", "Verdura","Cereal", "Biscoito"],
+    alimento: ["Fettuccine", "Parmegiana", "PIZZA", "Hamburguer", "Verdura","Cereal", "Biscoito"],
 };
